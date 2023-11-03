50e77353da93a14e396f5ec443c5c33340043292
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-03 23:09:27 +0000

    updating with layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-import { LandingPage } from "./pages/landingpage";
+import  LandingPage  from "./pages/landingpage";
 import { Home } from "./pages/home";
 
 import { Single } from "./pages/single";
