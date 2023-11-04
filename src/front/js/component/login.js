16c0908f3d331846508b4154a270a22e1014bbc8
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-04 00:13:27 +0000

    fixed login component

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,36 +1,48 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Login = (props) => {
-	const navigate = useNavigate();
-	const { store, actions } = useContext(Context);
-	const [ email, setEmail] = useState("");
-	const [ password, setPassword ] = useState("");
-	const onSubmit = async (event) => {
-		const success = await actions.logIn({
-			email: email,
-			hashed_password: password
-		});
-		if (success) {
-		navigate("/profile");
-		}
-	};
-	const onSignup = () => {
-		navigate("/sign-up")
-	}
+    const navigate = useNavigate();
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-	return (
-		<div className="text-center mt-5 top-right-nav">
-			<h1>Log in</h1>
-			<input className="form-control m-3" type="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}></input>
-			<input className="form-control m-3" type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}></input>
-			<button 
-				className="btn btn-primary m-2"
-				onClick={onSubmit}
-				>log in</button>
-			<button className="btn btn-success m-2" onClick={onSignup}>Sign Up!</button>
-		</div>
-	);
+    const onSubmit = async (event) => {
+        const success = await actions.logIn({ email: email, hashed_password: password });
+        if (success) {
+            navigate("/profile");
+        }
+    };
+
+    const onSignup = () => {
+        navigate("/sign-up");
+    };
+
+    return (
+        <div className="text-center mt-5 top-right-nav">
+            <h1>Log in</h1>
+            <input
+                className="form-control m-3"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            ></input>
+            <input
+                className="form-control m-3"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            ></input>
+            <button className="btn btn-primary m-2" onClick={onSubmit}>
+                Log in
+            </button>
+            <button className="btn btn-success m-2" onClick={onSignup}>
+                Sign Up!
+            </button>
+        </div>
+    );
 };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,27 +1,68 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import "../../styles/index.css"
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/index.css";
 import { Login } from "./login";
 import { LoggedIn } from "./loggedin";
 import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	const { store, actions } = useContext(Context)
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Home Bar Pro</span>
-				</Link>
-				<div className="ml-auto">
-					{store.accessToken !== undefined ? (
-						<LoggedIn />
-					) : (
-						<Login />
-					)}
-				</div>
-			</div>
-		</nav>
-	);
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleLogoClick = () => {
+        if (window.location.pathname === "/") {
+            window.location.reload();
+        } else {
+            navigate("/");
+        }
+    };
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <nav className="navbar custom-navbar-bg">
+            <div className="container d-flex justify-content-align-items-center">
+                <Link to="/" onClick={handleLogoClick}>
+                    <img
+                        src={CheerslogoCorner}
+                        alt="Home Bar Pro"
+                        className="navbar-logo"
+                        style={{ transform: 'scale(.7)' }}
+                    />
+                </Link>
+
+                <div className="tab-link-container">
+                    <Link to="/BlogPage" className="tab-link">
+                        Blog
+                    </Link>
+                    <Link to="/Social" className="tab-link">
+                        Social
+                    </Link>
+                    <Link to="/userfavorites" className="tab-link">
+                        Favorites
+                    </Link>
+                </div>
+
+                <div className="dropdown-container">
+                    <button onClick={toggleDropdown} className="dropbtn">
+                        Menu
+                    </button>
+                    {dropdownOpen && (
+                        <div className="dropdown-content">
+                            {store.accessToken !== undefined ? (
+                                <LoggedIn style={{ width: '300px', height: '300px' }} />
+                            ) : (
+                                <Login style={{ width: '300px', height: '300px' }} />
+                            )}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
 export default Navbar
