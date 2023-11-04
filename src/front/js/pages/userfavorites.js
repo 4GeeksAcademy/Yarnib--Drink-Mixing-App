bf056a94a97e1046e898e06e768d553a8cbe3f22
Drea Q <marcellab721@icloud.com>
2023-11-04 01:08:09 +0000

    changes

--- src/front/js/pages/userfavorites.js
+++ src/front/js/pages/userfavorites.js
@@ -22,50 +22,52 @@ export const UserFavorites = () => {
         <div className="sidebar container" >
             <div className="row justify-content-center">
                 <h1 className="text-center">
-                    <i className="fas fa-glass-martini-alt me-2"></i>
-                    <i className="fas fa-wine-glass me-2"></i>
-                    <i className="fas fa-beer me-2"></i>
-                    Favorites
-                    <i className="fas fa-beer ms-2"></i>
-                    <i className="fas fa-wine-glass ms-2"></i>
-                    <i className="fas fa-glass-martini-alt ms-2"></i>
+                    <i style={{ color: "white" }} className="fas fa-glass-martini-alt me-2"></i>
+                    <i style={{ color: "white" }} className="fas fa-wine-glass me-2"></i>
+                    <i style={{ color: "white" }} className="fas fa-beer me-2"></i>
+                    <text style={{ color: "white" }}>Favorites</text>
+                    <i style={{ color: "white" }} className="fas fa-beer ms-2"></i>
+                    <i style={{ color: "white" }} className="fas fa-wine-glass ms-2"></i>
+                    <i style={{ color: "white" }} className="fas fa-glass-martini-alt ms-2"></i>
                 </h1>
             </div>
-            {favs.length == 0 ? (
-                <div>
-                    <p className="text-center">
-                        Oh NOOOOO!!!! No favorite drinks... Let's change that!<br />
-                        <Link to="/">
-                            <span>Search For Drinks</span>
-                        </Link>
-                    </p>
-                </div>
-            ) : (favs.map((fav) => (
-                <div className="row" key={fav.id}>
-                    <div className="col-10">
-                        <a href="#" className="list-group-item list-group-item-action py-3 lh-tight" aria-current="true" style={{ background: "inherit" }}>
-                            <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong className="mb-1">{fav.name}</strong>
-                                <small>Wed</small>
-                            </div>
-                            <div className="col-10 mb-1 small"> </div>
-                        </a>
-                        <img style={{ width: 100, height: 100 }} src={fav.strDrinkThumb}></img>
+            {
+                favs.length == 0 ? (
+                    <div>
+                        <p className="text-center">
+                            <text style={{ color: "white" }}>No Favorites, yet! Click below to find your favorite drink</text><br />
+                            <Link to="/">
+                                <span>Search For Drinks</span>
+                            </Link>
+                        </p>
                     </div>
-                    <div className="col-2">
+                ) : (favs.map((fav) => (
+                    <div className="row" key={fav.id}>
+                        <div className="col-10">
+                            <a href="#" className="list-group-item list-group-item-action py-3 lh-tight" aria-current="true" style={{ background: "inherit" }}>
+                                <div className="d-flex w-100 align-items-center justify-content-between">
+                                    <strong className="mb-1">{fav.name}</strong>
+                                    <small>Wed</small>
+                                </div>
+                                <div className="col-10 mb-1 small"> </div>
+                            </a>
+                            <img style={{ width: 100, height: 100 }} src={fav.strDrinkThumb}></img>
+                        </div>
                         <div className="col-2">
-                            <button onClick={() => {
-                                actions.deleteFavorites(fav.id).then(e => {
-                                    setUpdate(!update)
-                                })
-                            }
-                            }>
-                                <i className="fa-solid fa-trash-can"></i>
-                            </button>
+                            <div className="col-2">
+                                <button onClick={() => {
+                                    actions.deleteFavorites(fav.id).then(e => {
+                                        setUpdate(!update)
+                                    })
+                                }
+                                }>
+                                    <i className="fa-solid fa-trash-can"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )))}
+                )))
+            }
 
         </div >
     )
