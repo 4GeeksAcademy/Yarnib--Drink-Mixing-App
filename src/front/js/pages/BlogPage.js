88859edd576a6edbfa37c88ebe49414e114d20d9
Yarnib <138246961+Yarnib@users.noreply.github.com>
2023-11-04 00:45:58 +0000

    update

--- src/front/js/pages/BlogPage.js
+++ src/front/js/pages/BlogPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import BlogList from './BlogList';
+import Chatbot from './chatbot';
+import ShotsHeader1080 from "../../img/Headerimages/ShotsHeader1080.jpg";
 
 const BlogPage = () => {
   return (
     <div className="blog-page">
       <h1 style={{ textAlign: 'center' }}>Shots</h1>
       <BlogList />
+
+      {/* Render the ChatBot component here, inside the return statement */}
+      <Chatbot />
     </div>
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../store/appContext';
 import rigoImageUrl from '../../img/rigo-baby.jpg';
 import '../../styles/home.css';
-import ChatBot from './ChatBot';
+import chatbot from './chatbot';
+import Blogsidebar from './Blogsidebar'; // Import the Blogsidebar component
 import { fetchCocktails, fetchCocktailsByIngredient, fetchCocktailByName } from './api';
-
+import Homebarprotopsplashnotitle from "../../img/Headerimages/Homebarprotopsplashnotitle.jpg";
 export const Home = () => {
   const { store, actions } = useContext(Context);
   const [cocktails, setCocktails] = useState([]);
@@ -14,17 +15,16 @@ export const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [showDrinkList, setShowDrinkList] = useState(true);
   const [previousSearchResults, setPreviousSearchResults] = useState([]);
-
   useEffect(() => {
-    fetchCocktails()
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`)
+      .then((response) => response.json())
       .then((data) => {
-        setCocktails(data);
+        setCocktails(data.drinks);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
-
   const handleDrinkClick = (drink) => {
     fetchCocktailByName(drink.strDrink)
       .then((data) => {
@@ -35,24 +35,29 @@ export const Home = () => {
         console.error('Error fetching cocktail details:', error);
       });
   };
-
   const toggleFavorite = (drink) => {
     if (favorites.includes(drink.idDrink)) {
       const updatedFavorites = favorites.filter((fav) => fav !== drink.idDrink);
+      actions.deleteFavorites(1, drink.idDrink).then((result) => {
+        console.log(result)
+      })
       setFavorites(updatedFavorites);
     } else {
-      setFavorites([...favorites, drink.idDrink]);
+      console.log(drink)
+      actions.addToFavorites(1, drink.idDrink, drink.strDrink, drink.strDrinkThumb).then((result) => {
+        setFavorites([...favorites, drink.idDrink]);
+      })
     }
   };
-
   const handleIngredientSearch = () => {
     if (keywords.length > 0) {
       const ingredientsString = keywords.join(',');
-      fetchCocktailsByIngredient(ingredientsString)
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientsString}`)
+        .then((response) => response.json())
         .then((data) => {
-          if (data.length > 0) {
+          if (data.drinks && data.drinks.length > 0) {
             setPreviousSearchResults([...searchResults]);
-            setSearchResults(data);
+            setSearchResults(data.drinks);
             setShowDrinkList(true);
           } else {
             alert('No search results found.');
@@ -63,7 +68,6 @@ export const Home = () => {
         });
     }
   };
-
   const backToCocktailList = () => {
     if (previousSearchResults.length > 0) {
       setSearchResults(previousSearchResults);
@@ -72,40 +76,38 @@ export const Home = () => {
       alert('No previous search results available.');
     }
   };
-
   const handleStarClick = (event, drink) => {
     event.stopPropagation(); // Prevent the click event from bubbling up to the <li>
     toggleFavorite(drink);
   };
-
   return (
-    <div className="text-center mt-5">
-      <div className="search-bar" style={{ textAlign: 'center' }}>
-        <input
-          type="text"
-          style={{
-            textAlign: 'center', // Center the text inside the input field
-          }}
-          placeholder="Type Ingredient Here"
-          value={keywords.join(' ')}
-          onChange={(e) => setKeywords(e.target.value.split(' '))}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleIngredientSearch();
-            }
-          }}
-        />
-        <button
-          style={{
-            marginLeft: '10px',
-          }}
-          onClick={handleIngredientSearch}
-        >
-          Search by Ingredient
-        </button>
-      </div>
-
-      <div className="search-results-container">
+    <div style={{ background: `url(${Homebarprotopsplashnotitle})`, backgroundSize: 'cover', height: '100vh' }}>
+      <div className="text-center">
+        <div className="search-bar" style={{ margin: '100px 0', textAlign: 'center' }}>
+          <input
+            type="text"
+            style={{
+              textAlign: 'center', // Center the text inside the input field
+            }}
+            placeholder="Type Ingredient Here"
+            value={keywords.join(' ')}
+            onChange={(e) => setKeywords(e.target.value.split(' '))}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleIngredientSearch();
+              }
+            }}
+          />
+          <button
+            style={{
+              marginLeft: '10px',
+            }}
+            onClick={handleIngredientSearch}
+          >
+            Search
+          </button>
+        </div>
+        {/* <div className="search-results-container">
         {showDrinkList && searchResults.length > 0 ? (
           <ul className="cocktail-list">
             {searchResults.map((cocktail) => (
@@ -132,44 +134,84 @@ export const Home = () => {
             ))}
           </ul>
         ) : null}
-      </div>
-
-      {selectedDrink && (
-        <div className="drink-details">
-          <img src={selectedDrink.Image} alt={selectedDrink.strDrink} className="cocktail-image" />
-          <p className="drink-name">
-            {selectedDrink.strDrink}
-            <button
-              className={favorites.includes(selectedDrink.idDrink) ? 'favorite active' : 'favorite'}
-              onClick={() => toggleFavorite(selectedDrink)}
-            >
-              ★
-            </button>
-          </p>
-          <div>
-            <p className="drink-ingredients">
-              <strong>Ingredients:</strong>
-              <ul>
-                {selectedDrink.Ingredients.map((ingredient, index) => (
-                  <li key={index}>
-                    {ingredient.ingredient} ({ingredient.measure})
-                  </li>
-                ))}
-              </ul>
+      </div> */}
+        <div className="search-results-container">
+          {showDrinkList && searchResults.length > 0 ? (
+            <ul className="cocktail-list">
+              {searchResults.map((cocktail) => (
+                <li key={cocktail.idDrink} className="cocktail-item" onClick={() => handleDrinkClick(cocktail)}>
+                  <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} className="cocktail-image" />
+                  <div className="drink-info">
+                    <p className="drink-name">
+                      {cocktail.strDrink}
+                      <br></br>
+                      <button
+                        className={favorites.includes(cocktail.idDrink) ? 'favorite active' : 'favorite'}
+                        onClick={(e) => handleStarClick(e, cocktail)} // Use handleStarClick for star icon
+                      >
+                        ★
+                      </button>
+                    </p>
+                    <p className="other-info">
+                      <a href="#" onClick={() => handleDrinkClick(cocktail)}>
+                        Details
+                      </a>
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+        {selectedDrink && (
+          <div
+            style={{
+              backgroundColor: 'black',
+              color: 'white',
+              maxWidth: '500px',
+              margin: '0 auto',
+              padding: '20px',
+              border: '1px solid white',
+            }}
+            className="drink-details"
+          >
+            <img src={selectedDrink.Image} alt={selectedDrink.strDrink} className="cocktail-image" />
+            <p className="drink-name">
+              {selectedDrink.strDrink}
+              <button
+                className={favorites.includes(selectedDrink.idDrink) ? 'favorite active' : 'favorite'}
+                onClick={() => toggleFavorite(selectedDrink)}
+              >
+                ★
+              </button>
             </p>
-            <p className="drink-instructions">
-              <strong>Instructions:</strong> {selectedDrink.Instructions}
+            <div>
+              <p className="drink-ingredients">
+                <strong>Ingredients:</strong>
+                <ul>
+                  {selectedDrink.Ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                      {ingredient.ingredient} ({ingredient.measure})
+                    </li>
+                  ))}
+                </ul>
+              </p>
+              <p className="drink-instructions">
+                <strong>Instructions:</strong> {selectedDrink.Instructions}
+              </p>
+            </div>
+            <p className="other-info">
+              <a href="#" onClick={() => setSelectedDrink(null)}>
+                Back to search results
+              </a>
             </p>
           </div>
-          <p className="other-info">
-            <a href="#" onClick={() => setSelectedDrink(null)}>
-              Back to search results
-            </a>
-          </p>
-        </div>
-      )}
-
-      <ChatBot />
+        )}
+      </div>
+      <div className="sidebar">
+        <Blogsidebar /> {/* Include the BlogSidebar component here */}
+      </div>
+      <chatbot />
     </div>
   );
 };
